405b7ee1388ea473419f5841b711e7501b20a17f
terry_shui <tshui3@uwo.ca>
2025-06-24 15:38:05 -0400

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
-
+console.log("start");
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
